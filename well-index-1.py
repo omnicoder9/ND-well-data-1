4d9501b8ed6c3ebcf2fcedb5ce981039934514dc
omnicoder9 <89420643+omnicoder9@users.noreply.github.com>
2023-10-21 01:54:41 -0400

    initial python scripts

---
+++ well-index-1.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from collections import Counter
+
+df = pd.read_csv('Well_Index\WellIndex.csv')
+attributes = []
+for col in df.columns:
+    attributes.append(col)
+#print(attributes)
+
+#firstFive = df.head()
+#print(firstFive)
+
+#count total number of wells
+
+#count number of distinct operators
+operators = df["CurrentOperator"]
+distinct_operators = []
+op_map = {}
+for op in operators:
+    if op not in distinct_operators:
+        distinct_operators.append(op)
+        op_map[op] = 1
+    else:
+        op_map[op]+=1
+print("There are " + str(len(distinct_operators)) + " distinct operators in ND.")
+largest_op = max(op_map, key=op_map.get)
+largest_op_well_count = max(op_map.values())
+print("The largest operatior in ND is " + largest_op + " with " + str(largest_op_well_count) + " wells.")
+
+largest_ops = Counter(op_map).most_common(20)
+print("The 20 largest operators in ND are:\n")
+for key, value in largest_ops:
+    print(key + ": " + str(value) + " wells")
+
+#find (largest, smallest) operators that are currently active
